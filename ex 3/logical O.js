5bda05f867a58f7da77c24e47ea07d1f2adb1513
mansormd <addbrobusiness@gmail.com>
2026-03-28 05:55:26 -1200

    ex 3

---
+++ ex 3/logical O.js
@@ -0,0 +1,10 @@
+console.log ("Other exercise")
+
+
+let isAdult = true 
+let hasPermission = false
+
+console.log (isAdult && hasPermission)
+console.log (isAdult || hasPermission)
+console.log (!isAdult)
+console.log (!hasPermission)
