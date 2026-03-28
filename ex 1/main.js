7bccae1985638eee804c2966bb36498df6914115
mansormd <addbrobusiness@gmail.com>
2026-03-27 21:58:31 -1200

    This is frist ex

---
+++ ex 1/main.js
@@ -0,0 +1,14 @@
+
+console.log ("This is js exercises only")
+
+
+let man = 20 
+let woman = 10
+
+console.log ( man + woman)
+console.log ( man = woman)
+console.log ( man / woman)
+console.log ( man * woman)
+console.log ( man - woman)
+console.log ( man % woman)
+
