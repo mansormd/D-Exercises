bb69f95c36e8663647ea40e017d287cf1a60283a
mansormd <addbrobusiness@gmail.com>
2026-03-28 05:33:13 -1200

    ex 2 update

---
+++ ex 2/main.js
@@ -0,0 +1,13 @@
+console.log ("Other exersice in dugsiiye repo")
+
+let man = 30
+let woman = "10"
+
+console.log ( man == woman)
+console.log ( man === woman)
+console.log ( man != woman)
+console.log ( man !== woman)
+console.log ( man > woman)
+console.log ( man < woman)
+console.log ( man >= woman)
+console.log ( man <= woman)  
