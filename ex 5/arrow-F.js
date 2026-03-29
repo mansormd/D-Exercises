4473e024eb514142453e37bc79c3974b626f558a
mansormd <addbrobusiness@gmail.com>
2026-03-29 05:30:48 -1200

    ex 5 added

---
+++ ex 5/arrow-F.js
@@ -0,0 +1,19 @@
+console.log ("Hello Dugsiiye")
+
+
+
+let text = (name) => {
+    console.log ("kuso dhawoow " + name)
+}
+
+text ("Dugsiiye")
+text ("gacma furan")
+text ("Farxad")
+
+let qiimo = (price,quantity) => {
+    console.log ("Total: " + price * quantity)
+}
+
+qiimo ( 10,9)
+qiimo ( 60,6)
+qiimo ( 5,3)
