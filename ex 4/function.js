6c9f9cca827447b143f2378a8359f4bc6ab8726a
mansormd <addbrobusiness@gmail.com>
2026-03-28 23:05:15 -1200

    ex 4 added

---
+++ ex 4/function.js
@@ -0,0 +1,22 @@
+console.log ("This is onather exersice in js")
+
+
+
+
+
+function dugsiiye (name) {
+    console.log ("Hello " + name)
+}
+
+dugsiiye ("dugsiiye")
+dugsiiye ("teacher hmouda")
+dugsiiye ("eng Omar")
+
+
+let laptop = function (price,quantity) {
+    console.log ("Total: $" + price * quantity)
+}
+
+laptop (90,10)
+laptop (60,30)
+laptop (30,60)
