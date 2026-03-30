64bafe703b557a2a6a1ebfd31cfb6cdb94f8e7aa
mansormd <addbrobusiness@gmail.com>
2026-03-30 00:15:54 -1200

    ex 7 added

---
+++ ex 7/A-M.js
@@ -0,0 +1,17 @@
+console.log ("Anther exercise with js")
+
+
+let names = ["laptop","mouse","pc","network","internet"]
+
+console.log (names)
+
+names.push ("distop")
+console.log (names)
+
+names.shift ("cali")
+console.log (names)
+
+names.unshift ("nour")
+console.log (names)
+
+console.log (names.length)
