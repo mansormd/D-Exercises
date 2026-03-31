ab5da05f9782a6b4335a684054ac1eaf1176fd8b
mansormd <addbrobusiness@gmail.com>
2026-03-31 08:05:59 -1200

    ex 8 added

---
+++ ex 8/Object.js
@@ -0,0 +1,12 @@
+console.log ("Hello dugsiiye anther exercise js")
+
+let person = {
+    name: "manssour",
+    age: 23,
+    city: "Afgooye"
+}
+
+console.log (person)
+console.log (person.city)
+console.log (person.name)
+console.log (person.age)
