72258cedb542d2be2a90e56e5197b76961193113
mansormd <addbrobusiness@gmail.com>
2026-03-31 18:44:56 -1200

    new exercise js

---
+++ ex 9/adding-a.js
@@ -0,0 +1,19 @@
+console.log ("Hello there new ex js")
+
+
+let person ={
+    Name: "mansourr",
+    Age: 23,
+    City: "Hodan",
+    gree: function (){
+        console.log ("what do you say about js")
+    }
+}
+
+console.log (person)
+
+person.Brithday = 2003
+
+console.log (person)
+
+console.log (person.gree())
