9163edce5c8752830f25eccdb4b572443175cbd0
mansormd <addbrobusiness@gmail.com>
2026-04-01 05:26:19 -1200

    new ex js added

---
+++ ex 10/Array-O.js
@@ -0,0 +1,26 @@
+console.log ("Hello there new ex js")
+
+
+let arday = [ 
+    {
+        name: "mansour",
+        class: "Ab121a",
+        age: 23,
+        id: "12214"
+    },
+    {
+        name: "Tman",
+        class: "Ab123a",
+        age: 24,
+        id: "15514"
+    }
+]
+
+console.log (arday)
+console.log (arday[0])
+console.log (arday[0].name)
+console.log (arday[0].id)
+console.log (arday[1])
+console.log (arday[1].age)
+console.log (arday[1].class)
+
