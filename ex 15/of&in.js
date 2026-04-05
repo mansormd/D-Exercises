3119c62ca46b2039eb0d6de936cdec58d66178b1
mansormd <addbrobusiness@gmail.com>
2026-04-05 10:51:04 -1200

    new  ex 15

---
+++ ex 15/of&in.js
@@ -0,0 +1,13 @@
+console.log ("new lesson for js with dugsiiye")
+
+
+
+let people= [
+    {name: "mansour", age: 23, class: "AB1221"},
+    {name: "mustaf", age: 26, class: "AC1421"},
+    {name: "mucaad", age: 30, class: "AD1229"}
+]
+
+for (key in people){
+    console.log (people[key])
+}
