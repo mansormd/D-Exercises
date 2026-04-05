5a8bd2aff5c750081e985ee36a5fc48636810128
mansormd <addbrobusiness@gmail.com>
2026-04-04 20:53:48 -1200

    other ex 12

---
+++ ex 12/loops2.js
@@ -0,0 +1,11 @@
+console.log ("new lesson for js with dugsiiye")
+
+
+
+
+const teacher = ["Omar","T-man","mohammed","shrf-din",]
+
+for (let dugsiiye =0; dugsiiye<teacher.length; dugsiiye++){
+    console.log(teacher[dugsiiye])
+}
+
