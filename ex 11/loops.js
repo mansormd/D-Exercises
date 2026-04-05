fff42159ebc10e3d7e9df070822e92476f4d02e6
mansormd <addbrobusiness@gmail.com>
2026-04-04 20:08:55 -1200

    new lesson with dugsiiye

---
+++ ex 11/loops.js
@@ -0,0 +1,15 @@
+console.log ("new lesson for js with dugsiiye")
+
+
+
+
+// for loops
+
+for ( let Tiro =1; Tiro<51; Tiro++){
+    console.log (Tiro)
+}
+
+
+for ( let Tiro =1; Tiro<101; Tiro++){
+    console.log (Tiro)
+}
