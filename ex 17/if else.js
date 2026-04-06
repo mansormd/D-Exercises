efdee4625aff8a85174071302dfd29863d769df6
mansormd <addbrobusiness@gmail.com>
2026-04-05 22:02:54 -1200

    added a new exercise

---
+++ ex 17/if else.js
@@ -0,0 +1,12 @@
+console.log ("new lesson for js with dugsiiye")
+
+
+let age =15;
+
+if(age > 11){
+    console.log ("waa caruur")
+}else if ( age > 14){
+    console.log ("waa qangaar")
+}else if (age > 15) {
+    console.log ("waa nin wayn")
+}
