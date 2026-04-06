bdf8f808fc861d2edf18ea2514c9f128e0d6426b
mansormd <addbrobusiness@gmail.com>
2026-04-05 21:23:12 -1200

    new ex added

---
+++ ex 16/if.js
@@ -0,0 +1,12 @@
+console.log ("new lesson for js with dugsiiye")
+
+
+let age =15; 
+
+if (age <=15){
+    console.log ("wad qangaantay awoowe")
+}else {
+    console.log ("wali ma aadan qangaarin")
+}
+
+
