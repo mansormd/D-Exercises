615e4f6e88ffa48a89822ea96a508da76d0d037b
mansormd <addbrobusiness@gmail.com>
2026-04-10 17:59:27 +0300

    new lesson added

---
+++ ex 19/forech.js
@@ -0,0 +1,8 @@
+console.log ("new lesson for js with dugsiiye")
+
+
+const dugsiiye = ["mc hamouda","eng omar","eng T-man","eng sh-diin"]
+
+dugsiiye.forEach(dugsiiye=> {
+    console.log ("name : " + dugsiiye)
+});
