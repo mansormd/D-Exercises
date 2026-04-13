381acc8c4384e88c04e5307de70406059234a6e6
mansormd <addbrobusiness@gmail.com>
2026-04-13 10:56:28 +0300

    new lesson added

---
+++ ex 20/map.js
@@ -0,0 +1,16 @@
+console.log ("new lesson for js with dugsiiye")
+
+
+
+
+// const dugsiiye = ["mc hamouda","eng omar","eng T-man","eng sh-diin"]
+
+// dugsiiye.forEach(dugsiiye=> {
+//     console.log ("name : " + dugsiiye)
+// });
+
+const numbers = [1,2,3,4,5]
+
+const newNumbers = numbers.map ((numbers,index)=>numbers *2)    
+
+console.log (newNumbers)
