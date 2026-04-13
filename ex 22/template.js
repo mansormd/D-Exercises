186f11aebf001b08f54582d942e16ff0a580697b
mansormd <addbrobusiness@gmail.com>
2026-04-13 11:51:52 +0300

    new lesson added

---
+++ ex 22/template.js
@@ -0,0 +1,29 @@
+console.log ("new lesson for js with dugsiiye")
+
+
+
+
+// const numbers = [1,2,3,4,5,6]
+// const sum = numbers.reduce ((Total,num)=> Total + num,0)
+
+// console.log (sum)
+
+
+// let name = "mansour"
+// let city = "Afgoi"
+
+// let OldMessage = "Hello " +name + "Welcome to " + city;
+
+// let newMessage = `Hello ${name} Welcome to ${city}`
+
+// console.log( newMessage)
+
+
+let frutis = ["canbe", "liindhanan","moos","iwm"]
+
+// let canbe = frutis[0]
+// let liindhanan = frutis[0]
+// let moos = frutis[0]
+
+let [canbe, liindhanan , moos, iwm] = frutis
+console.log (canbe)
