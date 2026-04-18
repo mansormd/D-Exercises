7fa097fd3a80aa25b42234d91cff9dd2af960011
mansormd <addbrobusiness@gmail.com>
2026-04-18 07:42:50 +0300

    added a new lesson

---
+++ ex 25/ES6.js
@@ -0,0 +1,29 @@
+console.log ("new lesson lang: js  with dugsiiye")
+
+
+
+// let dugsiiye = {name: "mansour", id: "Ad1221",level: "l3",age: 23};
+
+// let {name,id,age} = dugsiiye
+// console.log(name)
+// console.log(id)
+// console.log(age)
+
+
+// function greet (name="guest"){
+//     console.log(`Hello ${name}`)
+// }
+
+// greet("manour");
+
+// function sum(...numbers) {
+//     return numbers.reduce((total, num) => total + num, 0);
+// }
+// console.log(sum(1, 200, 30)); 
+
+
+const numbers = [1,2,3,4,5]
+const allNumbers = [...numbers,60,70,80,90,100]
+
+console.log(allNumbers)
+
