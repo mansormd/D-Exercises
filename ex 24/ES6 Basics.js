7e2c25d96ca9e4d6e9b94a9d36ee53c901d926da
mansormd <addbrobusiness@gmail.com>
2026-04-18 06:50:43 +0300

    added a new lesson

---
+++ ex 24/ES6 Basics.js
@@ -0,0 +1,17 @@
+console.log ("new lesson lang: js  with dugsiiye")
+
+
+
+// let dugsiiye = {name: "mansour", id: "Ad1221",level: "l3",age: 23};
+
+// let {name,id,age} = dugsiiye
+// console.log(name)
+// console.log(id)
+// console.log(age)
+
+
+function greet (name="guest"){
+    console.log(`Hello ${name}`)
+}
+
+greet("manour");
